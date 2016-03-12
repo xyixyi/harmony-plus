9b465161b5fdc131fd4affb58d29d29039c9915e
xycheng0106@gmail.com <xycheng0106@gmail.com>
2016-03-12 01:59:18 +0000

    travis ci passed?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'simplecov'
 SimpleCov.start
 
+require "codeclimate-test-reporter"
+ CodeClimate::TestReporter.start
+ 
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
