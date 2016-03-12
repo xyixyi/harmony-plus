c6bb121c7ccd4cfd82d38ec5ea58b1875f1ac6af
xycheng0106@gmail.com <xycheng0106@gmail.com>
2016-03-12 05:48:07 +0000

    finish iteration 1-2?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'simplecov'
 SimpleCov.start
 
-require "codeclimate-test-reporter"
- CodeClimate::TestReporter.start
- 
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
