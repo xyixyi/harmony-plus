91714e1c7281fd207e999fd5b65695f8fb260724
frankzhao3 <hpzhao3@berkeley.edu>
2016-03-22 07:18:03 +0000

    add unique to column email in the db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321092709) do
+ActiveRecord::Schema.define(version: 20160322070809) do
 
   create_table "logins", force: :cascade do |t|
     t.string   "admin"
@@ -31,5 +31,7 @@ ActiveRecord::Schema.define(version: 20160321092709) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  add_index "students", ["email"], name: "index_students_on_email", unique: true
 
 end
