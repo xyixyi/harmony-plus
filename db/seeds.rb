576ea924d00b62b76da0890b88924818f2f4639c
xycheng0106@gmail.com <xycheng0106@gmail.com>
2016-04-12 22:55:15 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ students = [{:first_name => 'Luke', :last_name => 'Wang', :email => 'zhaohp@hotm
 students.each do |student|
     Student.create!(student)
     
-User.create(email: 'test@hotmail.com', password: 'test')
+User.create(email: 'test@hotmail.com', password: 'testforgabe')
 end
